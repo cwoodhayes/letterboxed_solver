71fdfe2d8eb7c48e867a4943d146ad0ae5f99798
Conor Hayes <cwoodhayes@gmail.com>
2024-11-10 19:25:15 -0500

    improved benchmarks for new dict

--- benches/solver_benchmark.rs
+++ benches/solver_benchmark.rs
@@ -1,4 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use letterboxed_solver::dictionary::get_dictionary_reader_from_file;
 use letterboxed_solver::dictionary::smart_dict::SmartDictionary;
 use letterboxed_solver::solvers::a_star;
 use letterboxed_solver::NYTBoxPuzzle; // Ensure this path is correct
@@ -6,6 +7,11 @@ use letterboxed_solver::NYTBoxPuzzle; // Ensure this path is correct
 fn get_nyt_example() -> NYTBoxPuzzle {
     // nov_6_2024
     NYTBoxPuzzle::from_str(6, "erb uln imk jav").unwrap()
+}
+
+/// benchmarks use the big dict
+fn get_smart_dict(puzzle: &NYTBoxPuzzle) -> SmartDictionary {
+    SmartDictionary::new_from_file(&puzzle, get_dictionary_reader_from_file("words_alpha.txt"))
 }
 
 fn benchmark_a_star(c: &mut Criterion) {
@@ -22,7 +28,7 @@ fn benchmark_a_star(c: &mut Criterion) {
 fn benchmark_a_star_helper(c: &mut Criterion) {
     let puzzle = get_nyt_example();
     println!("{}", puzzle);
-    let dict = SmartDictionary::new(&puzzle);
+    let dict = get_smart_dict(&puzzle);
 
     c.bench_function("A* helper", |b| {
         b.iter(|| a_star::_helper(black_box(&puzzle), black_box(&dict)));
